4bb6ac46725aceb76120cbcbe135c720d424e79f
Abhinav Jha <abhinav_jha@hotmail.com>
2022-04-10 13:12:13 +0530

    deleted files
    db.sqlite3

--- register/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import  Register, Workshop_details
-
-# Register your models here.
-
-admin.site.register(Register)
-
-
-admin.site.register(Workshop_details)
